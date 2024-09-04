89592f094c961535263fc794b0ef389d0230fb01
hemant0621 <hemant.kumar.2335.h@gmail.com>
2024-09-04 10:49:34 +0530

    added frontend

---
+++ frontend/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+     <a href="/user" className=" no-underline text-inherit">Login</a>
+    </div>
+  );
+}
