bb8618f540d595cd9871f9ea1ad13ab3f56b1e7b
Divyansh Rai <ce.divyanshrai@gmail.com>
2024-09-07 23:58:25 +0530

    added

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-     <a href="/user" className=" no-underline text-inherit">Login</a>
+     <a href="/user" className=" no-underline text-inherit">Login/Signup</a>
     </div>
   );
 }
