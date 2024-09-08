230d91b63a8b9683c247343742466c473e8a723c
hemant0621 <hemant.kumar.2335.h@gmail.com>
2024-09-08 16:53:44 +0530

    added

---
+++ frontend/app/Employer/Dashboard/page.js
@@ -0,0 +1,15 @@
+"use client"
+import Dashboard from "@/components/Dashboard";
+import Navbar from "@/components/Navbar";
+
+export default function Home() {
+
+    return (
+        <>
+            <Navbar />
+            <Dashboard user={"Employer"} />
+        </>
+    )
+
+
+}
--- frontend/app/page.js
+++ frontend/app/page.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div>
      <a href="/user" className=" no-underline text-inherit">Login</a>
+     <a href="/Dashboard" className=" no-underline text-inherit">Dashboard</a>
     </div>
   );
 }
